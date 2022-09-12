64d0c97f2edb1a5fccdcc6bd3f840aef3524650a
Marcos Bazbih <marcosbaza100@gmail.com>
2022-09-12 21:34:29 +0300

    category page card styling and responsive design, not done

--- client/src/components/parts/category-page/ItemCard.jsx
+++ client/src/components/parts/category-page/ItemCard.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState, useContext, useRef } from "react";
 import { Link } from "react-router-dom";
-import { getAvgRating } from "../../../utils/getAvgRating";
-import { StyledItemCard } from "../../styles/parts/StyledItemCard";
-import { DataContext } from "../../../contexts/data-context";
-import { verifyUserFavorites, activateHeartIcon, addClassToHeart } from "../../../utils/favoritesList-functions";
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import { ThemeContext } from "../../../contexts/theme-context";
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import StarIcon from '@mui/icons-material/Star';
+import { getAvgRating } from "../../../utils/getAvgRating";
+import { StyledItemCard } from "../../styles/parts/StyledItemCard";
+import { DataContext } from "../../../contexts/data-context";
+import { ThemeContext } from "../../../contexts/theme-context";
+import { verifyUserFavorites, activateHeartIcon, addClassToHeart } from "../../../utils/favoritesList-functions";
 
 const ItemCard = ({ product }) => {
     const { user } = useContext(DataContext);
@@ -25,24 +25,24 @@ const ItemCard = ({ product }) => {
         <StyledItemCard mode={mode}>
             {product &&
                 <>
-                    <button className="heart-icon-btn" disabled={verifyUserFavorites(user)}
+                    <button className="heart-btn" disabled={verifyUserFavorites(user)}
                         onClick={() => activateHeartIcon(heartIcon, product)}>
                         <FavoriteIcon ref={heartIcon} className={`heart-icon ${addClassToHeart(user, favorites, product)}`}>
                         </FavoriteIcon>
                     </button>
                     <div className="card-info">
-                        <h1 className="card-name">{product.name}</h1>
-                        <div className="location-price-wrapper">
-                            <address className="card-location">{product.location}</address>
-                            <p className="card-price">{product.price[0]}$ - {product.price[1]}$</p>
+                        <div className="card-price">
+                            <p>{product.price[0]}$ - {product.price[1]}$</p>
                         </div>
+                        <h1>{product.name}</h1>
+                        <address>{product.location}</address>
                         <Box className="rating-wrapper" sx={{ '& > legend': { mt: 2 } }}>
-                            <p className="reviews">{rating ? `${product.rating.length} reviews` : "no reviews yet"}</p>
+                            <p>{rating ? `${product.rating.length} reviews` : "no reviews yet"}</p>
                             <Rating className="rating-stars" name="text-feedback" value={Number(rating)} precision={0.5} readOnly emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />} />
                         </Box>
                     </div>
                     <Link className="card-link" to={`/${product.name}`} state={product}>
-                        <img className="image" src={product.images[0]} alt="item-img" />
+                        <img src={product.images[0]} alt={product.name} />
                     </Link>
                 </>
             }
