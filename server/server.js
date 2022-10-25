a759f6b911b47d5454d05a86c6ad38b1b2413a6c
Marcos Bazbih <marcosbaza100@gmail.com>
2022-10-25 21:44:57 +0300

    changed production line in server.js

--- server/server.js
+++ server/server.js
@@ -38,6 +38,8 @@ app.use("/api/data", dataRoutes);
 app.use("/auth", usersRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-  }
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+};
